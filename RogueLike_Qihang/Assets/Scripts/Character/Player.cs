2a39afaef22f021cacec86911047acc23cc39708
QihangITB <qihang.li.7e7@itb.cat>
2024-11-13 20:01:11 +0100

    add player movement with input system

--- RogueLike_Qihang/Assets/Scripts/Character/Player.cs
+++ RogueLike_Qihang/Assets/Scripts/Character/Player.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class Player : MonoBehaviour
+public class Player : AEntity, InputControl.IPlayerActions
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float speed;
+    private InputControl _inputControl;
+    private Vector2 _inputMovement;
+    private Rigidbody2D _rb;
+
+    private void Awake()
     {
-        
+        _inputControl = new InputControl();
+        _inputControl.Player.SetCallbacks(this);
+        _rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        _rb.MovePosition(_rb.position + speed * Time.deltaTime * _inputMovement.normalized);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        _inputControl.Player.Enable();
     }
+
+    private void OnDisable()
+    {
+        _inputControl.Player.Disable();
+    }
+
+    public void OnMovement(InputAction.CallbackContext context)
+    {
+        //Leemos la configuracion de las teclas de movimiento. EX: 'w' = (0,1)
+        _inputMovement = context.ReadValue<Vector2>();
+        Debug.Log(_inputMovement);
+    }
+
 }
