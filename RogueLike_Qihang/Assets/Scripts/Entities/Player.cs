d5445e962735a478c4fbea44bd01ea1efe581239
QihangITB <qihang.li.7e7@itb.cat>
2024-11-19 19:59:59 +0100

    change animation

--- RogueLike_Qihang/Assets/Scripts/Entities/Player.cs
+++ RogueLike_Qihang/Assets/Scripts/Entities/Player.cs
@@ -5,20 +5,27 @@ using UnityEngine.InputSystem;
 
 public class Player : AEntity, InputControl.IPlayerActions
 {
+    const string AxisX = "X", AxisY = "Y", ParamIsMoving = "isMoving";
+
     [SerializeField] private float speed;
+
     private InputControl _inputControl;
     private Vector2 _inputMovement;
     private Rigidbody2D _rb;
+    private Animator _animator;
+    private Vector2 _lastInput;
 
     private void Awake()
     {
         _inputControl = new InputControl();
         _inputControl.Player.SetCallbacks(this);
         _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
     private void Update()
     {
         _rb.MovePosition(_rb.position + speed * Time.deltaTime * _inputMovement.normalized);
+        AnimationByDirection();
     }
 
     private void OnEnable()
@@ -36,6 +43,22 @@ public class Player : AEntity, InputControl.IPlayerActions
         //Leemos la configuracion de las teclas de movimiento. EX: 'w' = (0,1)
         _inputMovement = context.ReadValue<Vector2>();
         Debug.Log(_inputMovement);
+
+        _animator.SetBool(ParamIsMoving, _inputMovement != Vector2.zero);
+        if(_inputMovement != Vector2.zero)
+            SaveLastInput();
+    }
+
+    //La animacion de movimiento dependera del input del teclado
+    private void AnimationByDirection()
+    {
+        _animator.SetFloat(AxisX, _lastInput.x);
+        _animator.SetFloat(AxisY, _lastInput.y);
+    }
+
+    private void SaveLastInput()
+    {
+        _lastInput = _inputMovement;
     }
 
 }
