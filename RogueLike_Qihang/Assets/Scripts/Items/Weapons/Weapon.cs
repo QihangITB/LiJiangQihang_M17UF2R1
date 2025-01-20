24bde75bc96ac78a30cc840cf2425710429e5a10
QihangITB <qihang.li.7e7@itb.cat>
2025-01-20 02:49:53 +0100

    update data

--- RogueLike_Qihang/Assets/Scripts/Items/Weapons/Weapon.cs
+++ RogueLike_Qihang/Assets/Scripts/Items/Weapons/Weapon.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Progress;
-using static UnityEngine.GraphicsBuffer;
 
 public class Weapon : Item
 {
